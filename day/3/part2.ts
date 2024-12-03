a9bde6c6b3f4c32d74da2cfbf1e85dbc029c8114
Thomas Cicognani <thomas.cicognani@proton.me>
2024-12-03 22:36:05 +0100

    :sparkles: feat: day 3 part 2

---
+++ day/3/part2.ts
@@ -0,0 +1,20 @@
+const inputRaw: string = Deno.readTextFileSync("./day/3/input/input.txt");
+
+let memory = 0;
+
+for (
+  const regexArray of inputRaw.matchAll(/mul\((\d+?),(\d+?)\)/g)
+) {
+  if (regexArray) {
+    const doIndex: number = inputRaw.lastIndexOf("do()", regexArray.index);
+    const dontIndex: number = inputRaw.lastIndexOf("don't()", regexArray.index);
+
+    if (doIndex >= dontIndex) {
+      const a: number = Number.parseInt(regexArray[1]);
+      const b: number = Number.parseInt(regexArray[2]);
+      memory += a * b;
+    }
+  }
+}
+
+console.log(`Memory: ${memory}`);
