07d4e67f549eebf63167602c3dde720c25c86f50
Thomas Cicognani <thomas.cicognani@proton.me>
2024-12-07 00:20:39 +0100

    :white_check_mark: test: create unit tests for day 1

---
+++ day/1/day1.test.ts
@@ -0,0 +1,11 @@
+import { assertEquals } from "jsr:@std/assert";
+import { day1part1 } from "./part1.ts";
+import { day1part2 } from "./part2.ts";
+
+Deno.test("Day 1 part 1", () => {
+  assertEquals(day1part1(), 2113135);
+});
+
+Deno.test("Day 1 part 2", () => {
+  assertEquals(day1part2(), 19097157);
+});
--- day/1/part1.ts
+++ day/1/part1.ts
@@ -1,25 +1,27 @@
-const inputRaw: string = Deno.readTextFileSync("./day/1/input/input.txt");
+export function day1part1(): number {
+  const inputRaw: string = Deno.readTextFileSync("./day/1/input/input.txt");
 
-const columnLeft: number[] = [];
-const columnRight: number[] = [];
+  const columnLeft: number[] = [];
+  const columnRight: number[] = [];
 
-for (const line of inputRaw.split("\n")) {
-  const lineSplit: string[] = line.split(/ +/);
-  if (lineSplit.length !== 2) {
-    continue;
+  for (const line of inputRaw.split("\n")) {
+    const lineSplit: string[] = line.split(/ +/);
+    if (lineSplit.length !== 2) {
+      continue;
+    }
+
+    columnLeft.push(Number.parseInt(lineSplit[0]));
+    columnRight.push(Number.parseInt(lineSplit[1]));
   }
 
-  columnLeft.push(Number.parseInt(lineSplit[0]));
-  columnRight.push(Number.parseInt(lineSplit[1]));
+  columnLeft.sort();
+  columnRight.sort();
+
+  let distance = 0;
+
+  for (let i = 0; i < columnLeft.length; i++) {
+    distance += Math.abs(columnRight[i] - columnLeft[i]);
+  }
+
+  return distance;
 }
-
-columnLeft.sort();
-columnRight.sort();
-
-let distance = 0;
-
-for (let i = 0; i < columnLeft.length; i++) {
-  distance += Math.abs(columnRight[i] - columnLeft[i]);
-}
-
-console.log(`Distance: ${distance}`);
