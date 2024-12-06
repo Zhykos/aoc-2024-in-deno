4d529e7b99cf2f00cfb3a33811138c2f62ec3773
Thomas Cicognani <thomas.cicognani@proton.me>
2024-12-07 00:44:43 +0100

    :sparkles: feat: create day template

---
+++ day/_DAY_TEMPLATE_/day7.test.ts
@@ -0,0 +1,11 @@
+import { assertEquals } from "jsr:@std/assert";
+import { day7part1 } from "./day7part1.ts";
+import { day7part2 } from "./day7part2.ts";
+
+Deno.test("Day 7 part 1", () => {
+  assertEquals(day7part1(), 0);
+});
+
+Deno.test.ignore("Day 7 part 2", () => {
+  assertEquals(day7part2(), 0);
+});
---
+++ day/_DAY_TEMPLATE_/day7part1.ts
@@ -0,0 +1,7 @@
+export function day7part1(): number {
+  const inputRaw: string = Deno.readTextFileSync(
+    "./day/7/input/input.txt",
+  );
+
+  return 0;
+}
