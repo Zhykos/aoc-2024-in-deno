154743a146b5e43c6ce05c5b413ce2722a3fd61c
Thomas Cicognani <thomas.cicognani@proton.me>
2024-12-10 20:10:33 +0100

    :sparkles: feat: day 10 part 1

--- day/10/day10.test.ts
+++ day/10/day10.test.ts
@@ -2,10 +2,10 @@ import { assertEquals } from "jsr:@std/assert";
 import { day10part1 } from "./day10part1.ts";
 import { day10part2 } from "./day10part2.ts";
 
-Deno.test.ignore("Day 10 part 1", () => {
-  assertEquals(day10part1(), 0);
+Deno.test("Day 10 part 1", () => {
+  assertEquals(day10part1(), 512);
 });
 
-Deno.test.ignore("Day 10 part 2", () => {
+Deno.test("Day 10 part 2", () => {
   assertEquals(day10part2(), 0);
 });
--- day/10/day10part1.ts
+++ day/10/day10part1.ts
@@ -1,7 +1,113 @@
+import { distinctBy } from "@std/collections/distinct-by";
+
 export function day10part1(): number {
   const inputRaw: string = Deno.readTextFileSync(
     "./day/10/input/input.txt",
   );
 
-  return 0;
+  const heightMapMatrix: HeightMap[][] = [];
+
+  const lines: string[] = inputRaw.split("\n");
+
+  for (let line = 0; line < lines.length; line++) {
+    const lineString: string = lines[line];
+    if (lineString === "") {
+      continue;
+    }
+
+    const heightMapLine: HeightMap[] = [];
+    heightMapMatrix.push(heightMapLine);
+
+    for (let i = 0; i < lineString.length; i++) {
+      const heightMap = new HeightMap(Number.parseInt(lineString[i]), i, line);
+      heightMapLine.push(heightMap);
+    }
+  }
+
+  const map = new TopographicMap(heightMapMatrix);
+  return map.searchAllTrails();
+}
+
+class TopographicMap {
+  constructor(public heightMapMatrix: HeightMap[][]) {}
+
+  searchAllTrails(): number {
+    let score = 0;
+
+    for (let i = 0; i < this.heightMapMatrix.length; i++) {
+      for (let j = 0; j < this.heightMapMatrix[i].length; j++) {
+        if (this.heightMapMatrix[i][j].height === 0) {
+          const finalVisitedHeights: HeightMap[] = TopographicMap.searchTrail(
+            i,
+            j,
+            0,
+            this.clone(),
+          );
+
+          const uniqueVisitedHeights = distinctBy(
+            finalVisitedHeights,
+            (f) => `${f.x};${f.y}`,
+          );
+
+          score += uniqueVisitedHeights.length;
+        }
+      }
+    }
+
+    return score;
+  }
+
+  static searchTrail(
+    i: number,
+    j: number,
+    currentHeight: number,
+    map: TopographicMap,
+  ): HeightMap[] {
+    if (
+      i < 0 ||
+      i >= map.heightMapMatrix.length ||
+      j < 0 ||
+      j >= map.heightMapMatrix[i].length ||
+      map.heightMapMatrix[i][j].alreadyVisited ||
+      map.heightMapMatrix[i][j].height !== currentHeight
+    ) {
+      return [];
+    }
+
+    map.heightMapMatrix[i][j].alreadyVisited = true;
+
+    if (currentHeight === 9) {
+      return [map.heightMapMatrix[i][j]];
+    }
+
+    return [
+      ...TopographicMap.searchTrail(i - 1, j, currentHeight + 1, map.clone()),
+      ...TopographicMap.searchTrail(i + 1, j, currentHeight + 1, map.clone()),
+      ...TopographicMap.searchTrail(i, j - 1, currentHeight + 1, map.clone()),
+      ...TopographicMap.searchTrail(i, j + 1, currentHeight + 1, map.clone()),
+    ];
+  }
+
+  clone(): TopographicMap {
+    const heightMapMatrixClone: HeightMap[][] = [];
+
+    for (const heightMapLine of this.heightMapMatrix) {
+      const heightMapLineClone: HeightMap[] = [];
+      heightMapMatrixClone.push(heightMapLineClone);
+
+      for (const heightMap of heightMapLine) {
+        heightMapLineClone.push(
+          new HeightMap(heightMap.height, heightMap.x, heightMap.y),
+        );
+      }
+    }
+
+    return new TopographicMap(heightMapMatrixClone);
+  }
+}
+
+class HeightMap {
+  alreadyVisited = false;
+
+  constructor(public height: number, public x: number, public y: number) {}
 }
