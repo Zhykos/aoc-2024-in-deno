07e5a5a746603f8e62a465a6b1505197477a064f
Thomas Cicognani <thomas.cicognani@proton.me>
2024-12-11 20:09:51 +0100

    :sparkles: feat: day 11 part 1

--- day/11/day11.test.ts
+++ day/11/day11.test.ts
@@ -2,10 +2,10 @@ import { assertEquals } from "jsr:@std/assert";
 import { day11part1 } from "./day11part1.ts";
 import { day11part2 } from "./day11part2.ts";
 
-Deno.test.ignore("Day 11 part 1", () => {
-  assertEquals(day11part1(), 0);
+Deno.test("Day 11 part 1", () => {
+  assertEquals(day11part1(), 182081);
 });
 
-Deno.test.ignore("Day 11 part 2", () => {
+Deno.test("Day 11 part 2", () => {
   assertEquals(day11part2(), 0);
 });
--- day/11/day11part1.ts
+++ day/11/day11part1.ts
@@ -3,5 +3,36 @@ export function day11part1(): number {
     "./day/11/input/input.txt",
   );
 
-  return 0;
+  const line: string = inputRaw.split("\n")[0];
+  const stones: Stone[] = line.trim().split(" ").map((x) =>
+    new Stone(Number.parseInt(x.trim()))
+  );
+
+  for (let i = 0; i < 25; i++) {
+    for (let j = 0; j < stones.length; j++) {
+      if (stones[j].value === 0) {
+        stones[j].value = 1;
+      } else if (stones[j].value.toString().length % 2 === 0) {
+        const valueStr: string = stones[j].value.toString();
+        const length = valueStr.length;
+        const stone1 = new Stone(
+          Number.parseInt(valueStr.substring(0, length / 2)),
+        );
+        const stone2 = new Stone(
+          Number.parseInt(valueStr.substring(length / 2, length)),
+        );
+        stones[j] = stone1;
+        stones.splice(j + 1, 0, stone2);
+        j++;
+      } else {
+        stones[j].value = stones[j].value * 2024;
+      }
+    }
+  }
+
+  return stones.length;
+}
+
+class Stone {
+  constructor(public value: number) {}
 }
