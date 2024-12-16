5233ecb356d7b8a71feaf9f7325edbe9c0b5563d
Thomas Cicognani <thomas.cicognani@proton.me>
2024-12-16 23:04:11 +0100

    :recycle: test: set day 16 part 1 as "TOO LONG"
    to prevent it to be run with normal unit tests

--- day/16/day16.test.ts
+++ day/16/day16.test.ts
@@ -2,7 +2,7 @@ import { assertEquals } from "jsr:@std/assert";
 import { day16part1 } from "./day16part1.ts";
 import { day16part2 } from "./day16part2.ts";
 
-Deno.test("Day 16 part 1", () => {
+Deno.test("Day 16 part 1 TOO LONG", () => {
   assertEquals(day16part1(), 7036);
 });
 
