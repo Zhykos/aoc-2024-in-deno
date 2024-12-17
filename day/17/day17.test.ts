1bd0f2a577e84e760386bb4f4f4f3b15df2e70f5
Thomas Cicognani <thomas.cicognani@proton.me>
2024-12-17 22:49:25 +0100

    :sparkles: feat: day 17 part 1

--- day/17/day17.test.ts
+++ day/17/day17.test.ts
@@ -2,10 +2,10 @@ import { assertEquals } from "jsr:@std/assert";
 import { day17part1 } from "./day17part1.ts";
 import { day17part2 } from "./day17part2.ts";
 
-Deno.test.ignore("Day 17 part 1", () => {
-  assertEquals(day17part1(), 0);
+Deno.test("Day 17 part 1", () => {
+  assertEquals(day17part1(), "7,4,2,5,1,4,6,0,4");
 });
 
-Deno.test.ignore("Day 17 part 2", () => {
+Deno.test("Day 17 part 2", () => {
   assertEquals(day17part2(), 0);
 });
