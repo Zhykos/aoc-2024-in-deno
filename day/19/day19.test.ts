d67681bac87be3d78914a73648d7ba9cdbdcd04c
Thomas Cicognani <thomas.cicognani@proton.me>
2024-12-19 23:02:15 +0100

    :sparkles: feat: day 19 part 1

--- day/19/day19.test.ts
+++ day/19/day19.test.ts
@@ -2,8 +2,8 @@ import { assertEquals } from "jsr:@std/assert";
 import { day19part1 } from "./day19part1.ts";
 import { day19part2 } from "./day19part2.ts";
 
-Deno.test("Day 19 part 1", () => {
-  assertEquals(day19part1(), 6);
+Deno.test("Day 19 part 1 TOO LONG", () => {
+  assertEquals(day19part1(), 267);
 });
 
 Deno.test("Day 19 part 2", () => {
--- day/19/day19part1.ts
+++ day/19/day19part1.ts
@@ -6,6 +6,8 @@ export function day19part1(): number {
   const towels: string[] = [];
   const designs: string[] = [];
 
+  let maxDesignLength = Number.MIN_SAFE_INTEGER;
+
   for (const line of inputRaw.split("\n")) {
     if (line === "") {
       continue;
@@ -17,24 +19,38 @@ export function day19part1(): number {
       }
     } else {
       designs.push(line);
+      maxDesignLength = Math.max(maxDesignLength, line.length);
     }
   }
 
-  //console.log(towels);
-  //console.log(designs);
-
-  return designs.filter((design) => canBeMade(design, towels)).length;
+  const cache = new Map<string, boolean>();
+  return designs.filter((design) => {
+    cache.clear();
+    return canBeMade(design, towels, maxDesignLength, cache);
+  })
+    .length;
 }
 
-function canBeMade(wantedDesign: string, towels: string[]): boolean {
-  console.log("canBeMade", wantedDesign);
+function canBeMade(
+  wantedDesign: string,
+  towels: string[],
+  maxDesignLength: number,
+  cache: Map<string, boolean>,
+): boolean {
+  //console.log("canBeMade", wantedDesign);
   return towels.map((towel) => {
     if (wantedDesign.includes(towel)) {
+      if (cache.has(towel)) {
+        return cache.get(towel) as boolean;
+      }
+
       return canBeMadeWith(
         towel,
         wantedDesign,
         //towels.filter((x) => x !== towel),
         towels,
+        maxDesignLength,
+        cache,
       );
     }
     return false;
@@ -45,31 +61,75 @@ function canBeMadeWith(
   pattern: string,
   wantedDesign: string,
   remainingTowels: string[],
+  maxDesignLength: number,
+  cache: Map<string, boolean>,
 ): boolean {
+  if (pattern.length > maxDesignLength) {
+    cache.set(pattern, false);
+    return false;
+  }
+
   //console.log("canBeMadeWith", pattern, wantedDesign, remainingTowels);
   return remainingTowels.map((towel) => {
     if (pattern + towel === wantedDesign || towel + pattern === wantedDesign) {
+      cache.set(pattern, true); // Well...
       return true;
     }
 
     if (wantedDesign.includes(pattern + towel)) {
+      if (cache.has(pattern + towel)) {
+        return cache.get(pattern + towel) as boolean;
+      }
+
       return canBeMadeWith(
         pattern + towel,
         wantedDesign,
         //remainingTowels.filter((x) => x !== towel),
         remainingTowels,
+        maxDesignLength,
+        cache,
       );
     }
 
     if (wantedDesign.includes(towel + pattern)) {
+      if (cache.has(towel + pattern)) {
+        return cache.get(towel + pattern) as boolean;
+      }
+
       return canBeMadeWith(
         towel + pattern,
         wantedDesign,
         //remainingTowels.filter((x) => x !== towel),
         remainingTowels,
+        maxDesignLength,
+        cache,
       );
     }
 
+    cache.set(pattern, false);
     return false;
   }).reduce((acc, curr) => acc || curr, false);
 }
+
+/*function generateDesigns(towels: string[], maxDesignLength: number): string[] {
+  console.log("generateDesigns", towels, maxDesignLength);
+  const designs = new Set<string>();
+
+  // Towels can be used multiple times
+
+  const severalTowels: string[] = [];
+  for (let i = 0; i < 3; i++) { // 3 is arbitrary
+    severalTowels.push(...towels);
+  }
+
+  for (let i = 0; i < severalTowels.length; i++) {
+    for (let j = 0; j < severalTowels.length; j++) {
+      const design: string = severalTowels[i] + severalTowels[j];
+      if (design.length <= maxDesignLength) {
+        designs.add(design);
+      }
+    }
+  }
+
+  return Array.from(designs);
+}*/
