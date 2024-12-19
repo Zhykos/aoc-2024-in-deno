b580db140470f98abc05acb1e3cd5fae55994aac
Thomas Cicognani <thomas.cicognani@proton.me>
2024-12-19 21:56:00 +0100

    :construction: feat: day 19 part 1 barbarian style
    but crap

--- day/19/day19.test.ts
+++ day/19/day19.test.ts
@@ -2,10 +2,10 @@ import { assertEquals } from "jsr:@std/assert";
 import { day19part1 } from "./day19part1.ts";
 import { day19part2 } from "./day19part2.ts";
 
-Deno.test.ignore("Day 19 part 1", () => {
-  assertEquals(day19part1(), 0);
+Deno.test("Day 19 part 1", () => {
+  assertEquals(day19part1(), 6);
 });
 
-Deno.test.ignore("Day 19 part 2", () => {
+Deno.test("Day 19 part 2", () => {
   assertEquals(day19part2(), 0);
 });
--- day/19/day19part1.ts
+++ day/19/day19part1.ts
@@ -1,7 +1,75 @@
 export function day19part1(): number {
-  const _: string = Deno.readTextFileSync(
+  const inputRaw: string = Deno.readTextFileSync(
     "./day/19/input/input.txt",
   );
 
-  return 0;
+  const towels: string[] = [];
+  const designs: string[] = [];
+
+  for (const line of inputRaw.split("\n")) {
+    if (line === "") {
+      continue;
+    }
+
+    if (line.includes(",")) {
+      for (const towel of line.split(",")) {
+        towels.push(towel.trim());
+      }
+    } else {
+      designs.push(line);
+    }
+  }
+
+  //console.log(towels);
+  //console.log(designs);
+
+  return designs.filter((design) => canBeMade(design, towels)).length;
+}
+
+function canBeMade(wantedDesign: string, towels: string[]): boolean {
+  console.log("canBeMade", wantedDesign);
+  return towels.map((towel) => {
+    if (wantedDesign.includes(towel)) {
+      return canBeMadeWith(
+        towel,
+        wantedDesign,
+        //towels.filter((x) => x !== towel),
+        towels,
+      );
+    }
+    return false;
+  }).reduce((acc, curr) => acc || curr, false);
+}
+
+function canBeMadeWith(
+  pattern: string,
+  wantedDesign: string,
+  remainingTowels: string[],
+): boolean {
+  //console.log("canBeMadeWith", pattern, wantedDesign, remainingTowels);
+  return remainingTowels.map((towel) => {
+    if (pattern + towel === wantedDesign || towel + pattern === wantedDesign) {
+      return true;
+    }
+
+    if (wantedDesign.includes(pattern + towel)) {
+      return canBeMadeWith(
+        pattern + towel,
+        wantedDesign,
+        //remainingTowels.filter((x) => x !== towel),
+        remainingTowels,
+      );
+    }
+
+    if (wantedDesign.includes(towel + pattern)) {
+      return canBeMadeWith(
+        towel + pattern,
+        wantedDesign,
+        //remainingTowels.filter((x) => x !== towel),
+        remainingTowels,
+      );
+    }
+
+    return false;
+  }).reduce((acc, curr) => acc || curr, false);
 }
