03a1a174649b68f6d888df682372e7adaf038d21
Thomas Cicognani <thomas.cicognani@proton.me>
2024-12-20 21:48:32 +0100

    :construction: feat: start Dijkstra for Day 20 part 1

--- day/20/day20.test.ts
+++ day/20/day20.test.ts
@@ -2,10 +2,10 @@ import { assertEquals } from "jsr:@std/assert";
 import { day20part1 } from "./day20part1.ts";
 import { day20part2 } from "./day20part2.ts";
 
-Deno.test.ignore("Day 20 part 1", () => {
+Deno.test("Day 20 part 1", () => {
   assertEquals(day20part1(), 0);
 });
 
-Deno.test.ignore("Day 20 part 2", () => {
+Deno.test("Day 20 part 2", () => {
   assertEquals(day20part2(), 0);
 });
--- day/20/day20part1.ts
+++ day/20/day20part1.ts
@@ -1,7 +1,107 @@
+import { Dijkstra, NodeVertex, Vertex } from "../18/dijkstra-algorithm.ts";
+
 export function day20part1(): number {
-  const _: string = Deno.readTextFileSync(
+  const inputRaw: string = Deno.readTextFileSync(
     "./day/20/input/input.txt",
   );
 
+  const graphMatrix: GraphNodePosition[][] = [];
+  let startNode: GraphNodePosition | null = null;
+  let endNode: GraphNodePosition | null = null;
+
+  const lineSplit: string[] = inputRaw.split("\n");
+  for (let i = 0; i < lineSplit.length; i++) {
+    const line: string = lineSplit[i];
+    if (line === "") {
+      continue;
+    }
+
+    graphMatrix[i] = [];
+
+    for (let j = 0; j < line.length; j++) {
+      const graphNodePosition = new GraphNodePosition(line[j], j, i);
+      graphMatrix[i].push(graphNodePosition);
+
+      if (line[j] === "S") {
+        startNode = graphNodePosition;
+      } else if (line[j] === "E") {
+        endNode = graphNodePosition;
+      }
+    }
+  }
+
+  console.log(matrixToString(graphMatrix));
+
+  const dijkstra = new Dijkstra();
+  for (let i = 0; i < graphMatrix.length; i++) {
+    for (let j = 0; j < graphMatrix[0].length; j++) {
+      const node: GraphNodePosition = graphMatrix[j][i];
+
+      if (node.element === "#") {
+        continue;
+      }
+
+      // Get neighbors
+      const neighbors: GraphNodePosition[] = [];
+      if (i > 0) {
+        neighbors.push(graphMatrix[j][i - 1]);
+      }
+      if (i < graphMatrix[0].length - 1) {
+        neighbors.push(graphMatrix[j][i + 1]);
+      }
+      if (j > 0) {
+        neighbors.push(graphMatrix[j - 1][i]);
+      }
+      if (j < graphMatrix.length - 1) {
+        neighbors.push(graphMatrix[j + 1][i]);
+      }
+
+      dijkstra.addVertex(
+        new Vertex(
+          node.toString(),
+          neighbors.filter((neighbor) => neighbor.element !== "#").map((
+            neighbor,
+          ) =>
+            new NodeVertex(
+              neighbor.toString(),
+              neighbor.element === "#" ? Number.MAX_SAFE_INTEGER : 1,
+            )
+          ),
+          1,
+        ),
+      );
+    }
+  }
+
+  const result: string[] = dijkstra.findShortestWay(
+    startNode?.toString() as string,
+    endNode?.toString() as string,
+  );
+
+  console.log(result);
+  console.log(result[result.length - 1]);
+
   return 0;
+}
+
+class GraphNodePosition {
+  constructor(public element: string, public x: number, public y: number) {}
+
+  toString(): string {
+    return `${this.x},${this.y}`;
+  }
+}
+
+function matrixToString(graphMatrix: GraphNodePosition[][]): string {
+  let result = "";
+
+  for (let i = 0; i < graphMatrix.length; i++) {
+    for (let j = 0; j < graphMatrix[i].length; j++) {
+      result += graphMatrix[i][j].element;
+    }
+
+    result += "\n";
+  }
+
+  return result;
 }
